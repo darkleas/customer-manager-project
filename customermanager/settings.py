bc9eee5265b747339c855d858a2aa6da0118edb1
darkleas <58261666+darkleas@users.noreply.github.com>
2021-08-12 20:08:45 +0300

    Update settings.py

--- customermanager/settings.py
+++ customermanager/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+from decouple import config
 
 from pathlib import Path
 import os
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fvoiu*x7_+k5-17&$$h@!mka_q_5!c6%xxyxnus%1%$c)&p6y&'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
